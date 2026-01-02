c5c1d561edfef57e0a5447526e150a4e5b2ee6f5
marshal10 <marshalmaleane123@gmail.com>
2026-01-02 18:27:25 +0530

    Implementing time travel

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -30,7 +30,7 @@ function Board({ xIsNext, squares, onPlay }) {
     }
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "❌" : "0️⃣";
-    onPlay(nextSquares)
+    onPlay(nextSquares);
   }
 
   return (
@@ -58,12 +58,35 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history,nextSquares])
-    setXIsNext(!xIsNext)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+    setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <>
@@ -76,7 +99,7 @@ export default function Game() {
           />
         </div>
         <div className="game-info">
-          <ol>{/*TODO*/}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     </>
